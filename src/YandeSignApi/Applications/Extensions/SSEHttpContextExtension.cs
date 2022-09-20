5b81019dd4ceb6d45a5a7186d406f419cb17caaf
yanh1993 <891367701@qq.com>
2022-09-20 22:25:57 +0800

    up

---
+++ src/YandeSignApi/Applications/Extensions/SSEHttpContextExtension.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace YandeSignApi.Applications.Extensions
+{
+    public static class SSEHttpContextExtension
+    {
+        /// <summary>
+        /// 自定义的SSE消息对象实体
+        /// </summary>
+        public class SseMessageObject
+        {
+            public string MsgId { get; set; }
+            public string MsgData { get; set; }
+        }
+
+        //public static HttpResponseMessage BuildingSse(HttpRequestMessage request)
+        //{
+
+        //    System.Net.Http.HttpResponseMessage response = request.CreateResponse();
+        //    response.Content = new System.Net.Http.PushStreamContent((Action<Stream, HttpContent, TransportContext>)WriteToStream, new MediaTypeHeaderValue("text/event-stream"));
+        //    return response;
+        //}
+
+        //private static readonly ConcurrentDictionary<StreamWriter, StreamWriter> _streammessage = new ConcurrentDictionary<StreamWriter, StreamWriter>();
+        //public static void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        //{
+        //    StreamWriter streamwriter = new StreamWriter(outputStream);
+        //    _streammessage.TryAdd(streamwriter, streamwriter);
+        //}
+
+        public static async Task SSEInitAsync(this HttpContext ctx)
+        {
+            ctx.Response.Headers.Add("Content-Type", "text/event-stream");
+            await ctx.Response.Body.FlushAsync();
+        }
+
+        public static async Task SSESendCloseEventAsync(this HttpContext ctx)
+        {
+            await ctx.Response.WriteAsync("event: close\n");
+            await ctx.Response.WriteAsync("retry: 3\n");
+            await ctx.Response.WriteAsync("data: bye\n");
+
+            await ctx.Response.WriteAsync("\n");
+            await ctx.Response.Body.FlushAsync();
+        }
+    }
+}
--- src/YandeSignApi/Controllers/RedisTestController.cs
+++ src/YandeSignApi/Controllers/RedisTestController.cs
@@ -1,12 +1,21 @@
-﻿using InitQ.Attributes;
+﻿using InitQ.Abstractions;
+using InitQ.Attributes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RedisPublishAndSubHelper;
 using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using YandeSignApi.Applications.Extensions;
 using YandeSignApi.Applications.Redis;
 using YandeSignApi.Applications.RedisMq;
 using YandeSignApi.Models.Dtos;
+using static YandeSignApi.Applications.Extensions.SSEHttpContextExtension;
 
 namespace YandeSignApi.Controllers
 {
@@ -15,9 +24,43 @@ namespace YandeSignApi.Controllers
     /// </summary>
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class RedisTestController : Controller
+    public class RedisTestController : Controller, IRedisSubscribe
     {
-        private readonly IRedisOperationRepository _redisOperationRepository;
+        private readonly IRedisOperationRepository _redisOperationRepository;  
+
+        private static readonly ConcurrentDictionary<StreamWriter, StreamWriter> _streammessage = new ConcurrentDictionary<StreamWriter, StreamWriter>();
+
+        private static void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            StreamWriter streamwriter = new StreamWriter(outputStream);
+            _streammessage.TryAdd(streamwriter, streamwriter);
+        }
+
+        public static void SendSseMsg(SseMessageObject sseMsg)
+        {
+            MessageCallback(sseMsg);
+        }
+
+        private static void MessageCallback(SseMessageObject sseMsg)
+        {
+            foreach (var subscriber in _streammessage.ToArray())
+            {
+                try
+                {
+                    subscriber.Value.WriteLine(string.Format("id: {0}\n", sseMsg.MsgId));
+                    subscriber.Value.WriteLine(string.Format("event: {0}\n", "SetBusinessSettlerEvent"));
+                    subscriber.Value.WriteLine(string.Format("data: {0}\n\n", sseMsg.MsgData));
+                    subscriber.Value.Flush();
+                }
+                catch
+                {
+                    StreamWriter streamWriter;
+                    _streammessage.TryRemove(subscriber.Value, out streamWriter);
+                }
+            }
+        }
+
+        private HttpContext _ctx;
         /// <summary>
         /// RedisTestController
         /// </summary>
@@ -62,60 +105,56 @@ namespace YandeSignApi.Controllers
         }
 
         [HttpGet]
+        public HttpResponseMessage SetBusinessSettlerEvent(HttpRequestMessage request)
+        {
+            HttpResponseMessage response = request.CreateResponse();
+            response.Content = new System.Net.Http.PushStreamContent((Action<Stream, HttpContent, TransportContext>)WriteToStream, new MediaTypeHeaderValue("text/event-stream"));
+            return response;
+        }
+
+        public class MyPushStreamResult : ActionResult
+        {
+            public string ContentType { get; private set; }
+            public PushStreamContent Stream { get; private set; }
+            public MyPushStreamResult(PushStreamContent stream, string contentType)
+            {
+                Stream = stream;
+                ContentType = contentType;
+            }
+            public override async Task ExecuteResultAsync(ActionContext context)
+            {
+                var response = context.HttpContext.Response;
+                response.ContentType = ContentType;
+                await Stream.CopyToAsync(response.Body);
+            }
+        }
+
+
+        /// <summary>
+        /// TestMake
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<CallBackResult> TestMake()
+        {
+            CallBackResult jm = new CallBackResult();
+            var SysUserId = "84FO0SMD00J8D1ZF";
+            await _redisOperationRepository.HSETNX(SysUserId.ToString(), SysUserId, SysUserId);
+
+            await _redisOperationRepository.ListRightPushAsync(RedisMessageQueueKey.TestSubscribeQueue, SysUserId.ToString());
+
+            return jm;
+        }
+
+
+        [HttpGet]
         [Subscribe(RedisMessageQueueKey.TestSubscribeQueue)]
-        public async Task SetBusinessSettlerEvent(string msg)
+        public async Task TriggerEvent()
         {
-            var response = Response;
-
-            HttpContext.Response.Headers.Add("Content-Type", "text/event-stream");
-            await HttpContext.Response.WriteAsync($"id: {Guid.NewGuid().ToString()}\n");
-            await HttpContext.Response.WriteAsync($"event:SetBusinessSettlerEvent\n");
-            await HttpContext.Response.WriteAsync($"retry: 3\n");
-            await HttpContext.Response.WriteAsync($"data: {111}\r\r");
-
-            await HttpContext.Response.Body.FlushAsync();
-
-            Response.Body.Close();
-        }
-
-        [HttpGet("EnqueueMsg")]
-        public async Task<CallBackResult<string>> EnqueueMsgAsync(string rediskey, string redisValue)
-        {
-            var jm = new CallBackResult<string>();
-
-            try
-            {
-                long enqueueLong = default;
-                for (int i = 0; i < 1000; i++)
-                {
-                    enqueueLong = await MyRedisSubPublishHelper.EnqueueListLeftPushAsync(rediskey, redisValue + i);
-                }
-                jm.Success($"入队的数据长度:{enqueueLong}", "入队成功");
-            }
-            catch (Exception ex)
-            {
-                jm.Failed($"出队异常，原因：{ex.Message}");
-            }
-
-            return jm;
-        }
-
-        [HttpGet("DequeueMsg")]
-        public async Task<CallBackResult<string>> DequeueMsgAsync(string rediskey)
-        {
-
-            var jm = new CallBackResult<string>();
-
-            try
-            {
-                string dequeueMsg = await MyRedisSubPublishHelper.DequeueListPopRightAsync(rediskey);
-                jm.Success($"出队的数据是:{dequeueMsg}", "出队成功");
-            }
-            catch (Exception ex)
-            {
-                jm.Failed($"出队异常，原因：{ex.Message}");
-            }
-            return jm;
+            var sseMsg = new SseMessageObject();
+            sseMsg.MsgId = "1101";
+            sseMsg.MsgData = "自定义告警消息";
+            SendSseMsg(sseMsg);
         }
     }
 }
